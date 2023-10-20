af778821598aab035753c23661b564b75f40c872
coder <jokhendra@cynayd.com>
2023-10-20 12:32:42 +0530

    added new route nothing

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));  
