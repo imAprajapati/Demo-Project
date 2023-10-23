0110ee1351ece9bb04f1e5a4b099e5a211ccbb6f
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-10-23 14:33:02 +0530

    update index.js name with karishma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Jokhendra this is your first CICD Project!'));
+app.get('/', (req, res) => res.send('Hello Karishma this is your first CICD Project!'));
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
 
