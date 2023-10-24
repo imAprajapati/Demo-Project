5d9066a3349da88a59345a78c67eabb5d538ee1c
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-10-24 11:18:02 +0530

    made the change in index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Jokhendra and karishma this is your first CICD Project!'));
+app.get('/', (req, res) => res.send('Hello Jokhendra I am from Azure'));
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
 
