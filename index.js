80e7d1e8cee8aa706e05d2b565387634bacbfd34
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-10-24 12:02:56 +0530

    Update index.js aws

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Jokhendra I am from Azure'));
+app.get('/', (req, res) => res.send('Hello Jokhendra I am from AWS'));
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
 
