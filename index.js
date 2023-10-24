b27cb5b80ba52362b977908a83862e49ce0d6dc9
coder <jokhendramau777@gmail.com>
2023-10-24 13:41:03 +0530

    added redis functionalyty

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const redis = require('redis');
 
-app.get('/', (req, res) => res.send('Hello Everyone this is my first CICD project base on AWS Docker Jenkins'));
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379
+});
+
+client.on('error', (err) => {
+  console.log("Error " + err);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected');
+});
+client.connect();
+
+;
+
+app.get('/', async(req, res) => {
+  await client.set("name","jokhendra");
+  res.send('Hello Everyone this is my first CICD project base on AWS Docker Jenkins');
+});
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
 
