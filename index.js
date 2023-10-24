f21daedea8779bffd50102c20d2787ba4513d08a
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-10-24 12:47:46 +0530

    added new message in index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Jokhendra I am from AWS'));
+app.get('/', (req, res) => res.send('Hello Everyone this is my first CICD project base on AWS Docker Jenkins'));
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
 
