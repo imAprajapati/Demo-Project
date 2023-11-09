c48e1ae56c8643b2c9442472214661a539fa0085
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-11-09 16:50:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const port = 3000;
 
 
 app.get('/', async(req, res) => {
-  res.send('Hello Everyone this is my first CICD project base on AWS Docker Jenkins jokhendra');
+  res.json({
+    success:true,
+    message:"Hello world this is first CI/CD declerative pipeline"
+  });
 });
 
 app.get('/nothing',(req,res) => res.json({success: true, message: 'Hello Nothing!'}));
