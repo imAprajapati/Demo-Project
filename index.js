c721bacb9c36b9e44630af6b58bc6c3b96087a03
Jokhendra Prajapati <49206910+imAprajapati@users.noreply.github.com>
2023-11-09 05:51:04 +0000

    changes in index file

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const redis = require('redis');
 
-const client = redis.createClient({
-  host: '127.0.0.1',
-  port: 6379
-});
 
-client.on('error', (err) => {
-  console.log("Error " + err);
-});
-
-client.on('connect', () => {
-  console.log('Redis client connected');
-});
-client.connect();
-
-;
 
 app.get('/', async(req, res) => {
   await client.set("name","jokhendra");
